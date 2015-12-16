d33583864e7b3247c92d4d028285f7085fd5e28c
Jackson Morgan <jmorgan@stuy.edu>
2015-12-16 16:49:14 -0500

    program works but must add exception

---
+++ LabGUI/Driver.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+public class Driver extends JFrame{
+    public static void main(String[]args){
+	TempConversionWindow g = new TempConversionWindow();
+	g.setVisible(true);
+    }
+}
