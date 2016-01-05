1806ba0d846be2704765fec08fdefbc7e2022c92
Jackson Morgan <jmorgan@stuy.edu>
2016-01-04 19:21:36 -0500

    Sorts now exists

---
+++ hwSorts/Sorts.java
@@ -0,0 +1,56 @@
+public class Sorts{
+   public static void printArray(int[]data){
+       String s = "";
+       int i = 0;
+       if (data.length ==0){
+	   s += "[]";
+       }
+       while(i < data.length){
+	   if(i == 0){
+	       s +="[ " + data[i] + ",";
+	   }
+	   else if (i == data.length - 1){
+	       s += " " + data[i] + "]";
+	   }
+	   else{s += " " + data[i] + ",";
+	   }
+	   i ++;
+       }
+       if (data.length == 1){
+	   s = "[" + data[0] + "]";
+       }
+       System.out.print(s);
+	   
+   }
+   public static void insertionSort(int[]data){
+       int i = 1;
+       //int [] new = int[] data.length;
+       while (i < data.length){
+	   int w = i;
+	   while(w > 0 && data[w] < data[w - 1]){
+	       int r = data[w -1];
+	       data[w - 1] = data[w];
+	       data[w] = r;
+	       w --;
+	       
+	   }
+	   i ++;
+       }
+       
+	       
+	   
+      //your code here to make data re-order its elements
+      //from least to greatest just like we did in class
+      //same algorithm as the way you created your OrderedSuperArray
+   }
+
+
+     public static void main(String[]args){
+	 int[] arrayName = {1000, -43, -42, -42, 8, 6, 7, 7, 5, 3, 1000, -54, 0, 7, 9, -329239, -329239};
+	 // int[] arrayName = {4, 1, 3};
+	Sorts.printArray( arrayName);
+	System.out.println();
+	Sorts.insertionSort( arrayName);
+	Sorts.printArray( arrayName);
+	}
+}
